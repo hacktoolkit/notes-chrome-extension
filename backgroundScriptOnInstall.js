f280c3e7ae756fa28024c8459e671858c16362b1
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-12-18 14:14:17 -0800

    Additional Setup Code
    Next main task - pull the page URL when the extension is clicked on and fill this in for the URL field in the form.

--- backgroundScriptOnInstall.js
+++ backgroundScriptOnInstall.js
@@ -1,1 +1,6 @@
-chrome.runtime.onInstalled.addListener(function () {});
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
+    let url = tabs[0].url;
+    console.log(url);
+  });
+});
