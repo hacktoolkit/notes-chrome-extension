f8ea7b2b889fb6a60bdb91d31f3a0604c9891b2c
Sivam Agarwalla <sivam.agarwalla@gmail.com>
2020-12-20 13:36:44 -0800

    Updated UI for the Add Note Page (#11)
    Changes
    -Add image field, URL Field, and Title Field
    -Added labels and adjusted the styling to look a bit better
    -URL, Image, and Title are autopopulated from the tab

--- src/js/popup.js
+++ src/js/popup.js
@@ -3,6 +3,8 @@ $(function () {
     const KEY_NOTES_URLS = '__NOTES_URLS__';
 
     let CURRENT_URL = null;
+    let CURRENT_TITLE = null;
+    let CURRENT_IMAGE_ICON = null;
     let NOTES_URLS = null;
 
     function showSuccessAlert() {
@@ -14,28 +16,6 @@ $(function () {
     function showErrorAlert() {
         $('.alerts').html(
             '<div class="alert alert-danger" role="alert">Fail :(</div>'
-        );
-    }
-
-    function getPreviouslySavedNotes() {
-        // https://developer.chrome.com/docs/extensions/reference/storage/
-        chrome.storage.sync.get(
-            [KEY_NOTES_URLS, CURRENT_URL],
-            function (result) {
-                // re-populate notes for current page
-                const value = result[CURRENT_URL];
-                $('#notes-content').val(value);
-
-                // re-populate all previous URLs
-                const urls = result[KEY_NOTES_URLS] || {};
-                NOTES_URLS = urls;
-
-                const urlLinks = _.map(_.keys(NOTES_URLS), (url) => {
-                    return '<li><a href="' + url + '">' + url + '</a></li>';
-                });
-
-                $('#notes-urls').html('<ul>' + urlLinks.join('') + '</ul>');
-            }
         );
     }
 
@@ -64,11 +44,52 @@ $(function () {
         }
     }
 
+    function getPreviouslySavedNotes() {
+        // https://developer.chrome.com/docs/extensions/reference/storage/
+        chrome.storage.sync.get(
+            [KEY_NOTES_URLS, CURRENT_URL],
+            function (result) {
+                // re-populate notes for current page
+                const value = result[CURRENT_URL];
+                $('#notes-content').val(value);
+
+                // re-populate all previous URLs
+                const urls = result[KEY_NOTES_URLS] || {};
+                NOTES_URLS = urls;
+
+                const urlLinks = _.map(_.keys(NOTES_URLS), (url) => {
+                    return (
+                        '<li class="list-group-item"><a href="' +
+                        url +
+                        '">' +
+                        url +
+                        '</a></li>'
+                    );
+                });
+
+                $('#notes-urls').html(
+                    '<ul class="list-group">' + urlLinks.join('') + '</ul>'
+                );
+            }
+        );
+    }
+
     function getActiveWindowTabURL() {
         chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
             const url = tabs[0].url;
-
+            const tabTitle = tabs[0].title;
+            const tabIconImage = tabs[0].favIconUrl;
+            console.log(tabs[0]);
             CURRENT_URL = url;
+            CURRENT_TITLE = tabTitle;
+            CURRENT_IMAGE_ICON = tabIconImage;
+            console.log(CURRENT_IMAGE_ICON);
+            console.log(typeof CURRENT_IMAGE_ICON);
+
+            $('#currentTabURL').val(CURRENT_URL);
+            $('#currentTabTitle').val(CURRENT_TITLE);
+            $('#currentTabImage').attr('src', CURRENT_IMAGE_ICON);
+
             getPreviouslySavedNotes();
         });
     }
